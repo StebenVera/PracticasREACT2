3649d505c0aae0a99c4d4851d75b14ee36a5b354
Ing.StebenAragon <stebenvera@gmail.com>
2019-01-21 01:37:50 -0500

    Avanzamos hasta estilos css con react

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,15 +1,17 @@
 import React,{Component} from 'react';
-
+import './media.css'
 
 
 class Media extends Component{
     render(){
+      
         return(
-            <div>
-                <div>
-                    <img src="" alt="" width={260} height={160}/>
-                    <h3>Buscandote</h3>
-                    <p>Mike bahia</p>
+            <div className="Media">
+                <div className="Media-cover">
+                    <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+                    <h3 className="Media-title">Buscandote</h3>
+                    <p className="Media-author">Mike bahia</p>
+                    
                 </div>
             </div>
         )
